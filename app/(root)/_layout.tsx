a1d8cc2b2ee965f946b25e42bdf16dde3285f28e
Jean-MarcCheynier <jm@cheynier.com>
2023-08-28 10:54:31 +0200

    feat(icon-tab-bar): set icon and color

--- app/(root)/_layout.tsx
+++ app/(root)/_layout.tsx
@@ -1,28 +1,36 @@
 import { Ionicons } from "@expo/vector-icons";
-import { Slot, Tabs } from "expo-router";
-import { Text } from "react-native";
+import {
+  Slot,
+  Tabs,
+  useNavigation,
+  useRootNavigation,
+  useRootNavigationState,
+  useRouter,
+} from "expo-router";
+import { Text, View } from "react-native";
 
 export default function HomeLayout() {
   return (
-    <Tabs
-      screenOptions={({ route }) => ({
-        tabBarIcon: ({ focused, color, size }) => {
-          let iconName;
+    <>
+      <Tabs
+        screenOptions={({ route }) => ({
+          tabBarIcon: ({ focused, color, size }) => {
+            let iconName;
 
-          if (route.name === "Home") {
-            iconName = focused
-              ? "ios-information-circle"
-              : "ios-information-circle-outline";
-          } else if (route.name === "Settings") {
-            iconName = focused ? "ios-list" : "ios-list-outline";
-          }
+            if (route.name === "home") {
+              iconName = focused
+                ? "ios-information-circle"
+                : "ios-information-circle-outline";
+            } else if (route.name === "profile") {
+              iconName = focused ? "ios-list" : "ios-list-outline";
+            }
 
-          // You can return any component that you like here!
-          return <Ionicons name={"time"} size={24} color={"blue"} />;
-        },
-        tabBarActiveTintColor: "tomato",
-        tabBarInactiveTintColor: "gray",
-      })}
-    />
+            return <Ionicons name={iconName} size={size} color={color} />;
+          },
+          tabBarActiveTintColor: "tomato",
+          tabBarInactiveTintColor: "gray",
+        })}
+      />
+    </>
   );
 }
