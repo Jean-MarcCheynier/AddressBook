eaeb728257e8076ce55a8a50e262cc937e553f7d
Jean-MarcCheynier <jm@cheynier.com>
2023-09-27 22:14:40 +0200

    feat(tamagui): init tamagui config

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,6 +1,9 @@
-import { Ionicons } from "@expo/vector-icons";
-import { Slot, Tabs } from "expo-router";
+import { Slot } from "expo-router";
 import { Provider } from "../context/auth/auth";
+import { TamaguiProvider } from "tamagui";
+import config from "../tamagui.config";
+import { useFonts } from "expo-font";
+import { useEffect } from "react";
 
 export const unstable_settings = {
   // Ensure any route can link back to `/`
@@ -8,9 +11,26 @@ export const unstable_settings = {
 };
 
 export default function HomeLayout() {
+  const [loaded] = useFonts({
+    Inter: require("@tamagui/font-inter/otf/Inter-Medium.otf"),
+    InterBold: require("@tamagui/font-inter/otf/Inter-Bold.otf"),
+  });
+
+  useEffect(() => {
+    if (loaded) {
+      // can hide splash screen here
+    }
+  }, [loaded]);
+
+  if (!loaded) {
+    return null;
+  }
+
   return (
-    <Provider>
-      <Slot />
-    </Provider>
+    <TamaguiProvider config={config}>
+      <Provider>
+        <Slot />
+      </Provider>
+    </TamaguiProvider>
   );
 }
